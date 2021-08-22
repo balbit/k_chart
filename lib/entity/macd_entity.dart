2f86e1b813b103d047ebef1eb1fa493971ab8bed
balbit <elliotliu123@gmail.com>
2021-08-22 22:41:37 +0800

    Update macd_entity.dart

--- lib/entity/macd_entity.dart
+++ lib/entity/macd_entity.dart
@@ -3,7 +3,7 @@ import 'kdj_entity.dart';
 import 'rsi_entity.dart';
 import 'rw_entity.dart';
 
-mixin MACDEntity on KDJEntity, RSIEntity, WREntity, CCIEntity {
+mixin MACDEntity on KDJEntity, RSIEntity, WREntity, CCIEntity, ModelEntity {
   double? dea;
   double? dif;
   double? macd;
