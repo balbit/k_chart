09196cd570271504b6e6e14671dc140e82a326ca
balbit <elliotliu123@gmail.com>
2021-09-12 11:21:34 +0800

    Update main_renderer.dart

--- lib/renderer/main_renderer.dart
+++ lib/renderer/main_renderer.dart
@@ -208,7 +208,7 @@ class MainRenderer extends BaseChartRenderer<CandleEntity> {
       Canvas canvas, double lastX, double curX) {
     if (lastPoint.model != 0) {
       drawLine(lastPoint.model, curPoint.model, canvas, lastX, curX,
-          this.chartColors.ma5Color);
+          Colors.yellowAccent, strokeWidth = 2.0);
     }
   }
 
